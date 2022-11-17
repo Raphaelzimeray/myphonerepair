75b3ba30da7df2c741ad7c4a7be3e95189f41eb2
Raphael Zimeray <raphaelzimeray@me.com>
2022-11-17 11:34:27 +0100

    data protection

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+require 'open-uri'
+
+
+puts "Cleaning database"
+
+Phone.destroy_all
+
+puts 'Creating 10 phones'
+
+iphone14pro = Phone.new(
+  brand: "Apple",
+  model: "I Phone 14 Pro",
+  capacity: 256,
+  color: "Violet",
+  camera: "48 Mpx",
+  year: 2022,
+  processor: "A16 Bionic",
+  price: 1459
+)
+
+file = URI.open('')
+iphone14pro.photos.attach(io: file, filename: '', content_type: '')
+iphone14pro.save!
+puts "#{iphone14pro.model} has been created!"
