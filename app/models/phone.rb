dcf2fe6ce31fa683cbecd81b6b01b9df8b89a929
Raphael Zimeray <raphaelzimeray@me.com>
2022-11-19 19:54:57 +0100

    phone create and new are working!

--- app/models/phone.rb
+++ app/models/phone.rb
@@ -1,3 +1,11 @@
 class Phone < ApplicationRecord
   has_one_attached :photo
+  validates :brand, presence: true
+  validates :model, presence: true
+  validates :capacity, presence: true
+  validates :color, presence: true
+  validates :camera, presence: true
+  validates :year, presence: true
+  validates :processor, presence: true
+  validates :price, presence: true
 end
