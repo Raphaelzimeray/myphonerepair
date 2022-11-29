e58212faaddf73a50115d5e7dc1b9c27a4996edc
Raphael Zimeray <raphaelzimeray@me.com>
2022-11-29 14:52:09 +0100

    phone reparation new

--- app/controllers/reparations_controller.rb
+++ app/controllers/reparations_controller.rb
@@ -11,10 +11,25 @@ class ReparationsController < ApplicationController
 
   def new
     @reparation = Reparation.new
-    @phone = 
+    @phone = Phone.find(params[:phone_id])
   end
 
   def create
+    @reparation = Reparation.new(reparation_params)
+    @reparation.user = current_user
+    @reparation.phone = Phone.find(params[:phone_id])
+    @phone = Phone.find(params[:phone_id])
+      if @reparation.save
+        redirect_to reparation_path(@reparation)
+      else
+        render :new
+      end
+  end
+
+  private
+
+  def reparation_params
+    params.require(:reparation).permit(:date, :price_of_repair)
   end
 
 end
