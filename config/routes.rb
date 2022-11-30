c14bb2b43fccc17cf54d32ae546edcb8dd2abc92
Raphael Zimeray <raphaelzimeray@me.com>
2022-11-30 09:08:22 +0100

    en cours

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   resources :phones, only: [ :index, :show, :new, :create ] do
-    resources :reparations, only: [ :index, :show, :new, :create]
+    resources :reparations, only: [ :new, :create]
   end
+  resources :reparations, only: [:index, :show ]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
