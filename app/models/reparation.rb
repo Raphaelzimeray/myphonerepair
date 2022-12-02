4e668faa6b8c2e9cb7ed9cdb2fd6ca1a9c5b4c64
Raphael Zimeray <raphaelzimeray@me.com>
2022-12-02 19:46:21 +0100

    trying to fix bug

--- app/models/reparation.rb
+++ app/models/reparation.rb
@@ -2,5 +2,6 @@ class Reparation < ApplicationRecord
   belongs_to :phone
   validates :date, presence: true
   validates :price_of_repair, presence: true
-  LISTE_REPARATIONS =  ["Ecran", "Appareil photo", "Batterie", "Haut parleur", "Connecteur de charge", "Facade arrière"]
+  validates :reparation_type, presence: true
+  LISTE_REPARATIONS = ["Ecran", "Appareil photo", "Batterie", "Haut parleur", "Connecteur de charge", "Facade arrière"]
 end
